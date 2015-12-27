01a10f71b3875e97a11fd8f4c581d44c4550cfbf
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2015-12-27 09:42:59 -0800

    AlchemyHttpMock interface updates

--- src/main/java/tech/sirwellington/alchemy/http/mock/AlchemyHttpMock.java
+++ src/main/java/tech/sirwellington/alchemy/http/mock/AlchemyHttpMock.java
@@ -1,18 +1,18 @@
-/*
- * Copyright 2015 SirWellington Tech.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ /*
+  * Copyright 2015 SirWellington Tech.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *      http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 
 package tech.sirwellington.alchemy.http.mock;
 
@@ -34,7 +34,7 @@ import static tech.sirwellington.alchemy.arguments.assertions.Assertions.instanc
  */
 @FluidAPIDesign
 public interface AlchemyHttpMock
-{ 
+{
     
     static When begin()
     {
@@ -48,51 +48,54 @@ public interface AlchemyHttpMock
             .usingMessage("Can only verify with AlchemyHttp generated from AlchemyHttpMock")
             .is(instanceOf(MockAlchemyHttp.class));
     }
-
+    
     interface When
     {
-
+        
         AlchemyHttp build();
-
-        Then whenPost();
-
-        Then whenGet();
-
-        Then whenPut();
-
-        Then whenDelete();
-
+        
+        Body whenPost();
+        
+        Body whenGet();
+        
+        Body whenPut();
+        
+        Body whenDelete();
+        
     }
     
     interface Body
     {
+        
+        At noBody();
+        
         At body(@NonNull Object pojo);
         
         At body(@NonNull JsonElement jsonBody);
         
         At body(@NonEmpty String jsonString);
         
-        At noBody();
     }
     
     interface At
     {
+        
         Then at(@NonEmpty String url);
     }
-
+    
     interface Then
     {
-
+        
         When thenDo(@NonNull Callable<?> operation);
-
+        
         When thenThrow(@NonNull Throwable ex);
-
+        
         When thenReturn(@Nullable Object pojo);
-
+        
         When thenReturnJson(JsonElement json);
-
+        
         When thenReturnResponse(HttpResponse response);
-
+        
     }
-
+    
 }
